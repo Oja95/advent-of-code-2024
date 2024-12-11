69b7e1f4fe604d68fd0bd6134f4b7670f8b13018
Tiit Oja <tiit.oja@xolo.io>
2024-12-11 08:48:01 +0200

    Refactor day 11 solution

--- src/day/day11.rs
+++ src/day/day11.rs
@@ -4,11 +4,9 @@ use crate::day::utils;
 
 pub fn run() {
     let input_string = utils::read_input(11);
-    // println!("{}", run_part_one(&input_string, 6));
-    // println!("{}", run_part_one(&input_string, 25));
+    println!("{}", run_part_one(&input_string, 25));
     println!("{}", run_part_two(&input_string, 75));
 }
-
 
 fn run_part_one(input_string: &str, blinks: usize) -> usize {
     // run part 1 with iteration to get the hang of the logic, then use maths :TM: or DP to solve
@@ -19,18 +17,16 @@ fn run_part_one(input_string: &str, blinks: usize) -> usize {
         .collect_vec();
 
     for _ in 0..blinks {
-        println!("{:?} {}", stones, stones.len());
-
         let mut j = 0;
         while j < stones.len() {
-            let mut stone = stones[j];
+            let stone = stones[j];
             if stone == 0 {
                 stones[j] = 1;
                 j += 1;
                 continue;
             }
 
-            let digits_count = num_digits(stone);
+            let digits_count = (stone as f64).log10().floor() as usize + 1;
             if digits_count % 2 == 0 {
                 let split = 10_usize.pow((digits_count / 2) as u32);
                 stones.insert(j + 1, stone % split); // linear time
@@ -44,34 +40,20 @@ fn run_part_one(input_string: &str, blinks: usize) -> usize {
         }
     }
 
-    println!("{:?}", stones);
     stones.len()
 }
 
-fn run_part_two(input_string: &str, blinks: u128) -> u128 {
-    let mut stones = input_string.split_whitespace()
-        .map(|s| s.parse::<u128>().unwrap())
-        .collect_vec();
-
-    // Use a hashmap for DP lookup to map (elem, blinks) -> how many stones it will produce
-    // mapping
-
-    let mut stones_sum = 0;
-
+fn run_part_two(input_string: &str, blinks: usize) -> usize {
+    // Memoization map storing (elem, blinks) -> (resulting stones count)
     let mut dp_map = HashMap::new();
 
-    for stone in stones {
-        stones_sum += dp(stone, blinks, &mut dp_map);
-    }
-
-    println!("{:?}", dp_map);
-
-    stones_sum
+    input_string.split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .map(|stone| find_stone_count(stone, blinks, &mut dp_map))
+        .sum()
 }
 
-// (elem, blinks) -> (stones count)?
-// calculate stones count for a elem after given amount of iterations
-fn dp(elem: u128, iterations: u128, dp_map: &mut HashMap<(u128, u128), u128>) -> u128 {
+fn find_stone_count(elem: usize, iterations: usize, dp_map: &mut HashMap<(usize, usize), usize>) -> usize {
     if iterations == 0 {
         return 1;
     }
@@ -80,34 +62,24 @@ fn dp(elem: u128, iterations: u128, dp_map: &mut HashMap<(u128, u128), u128>) ->
         return *result;
     }
 
-    let result: u128;
+    let result;
     if elem == 0 {
-        result = dp(1, iterations - 1, dp_map);
+        result = find_stone_count(1, iterations - 1, dp_map);
     } else {
-        let digits_count = num_digits_u128(elem);
+        let digits_count = (elem as f64).log10().floor() as usize + 1;
         if digits_count % 2 == 0 {
-            let split = 10_u128.pow((digits_count / 2) as u32);
+            let split = 10_usize.pow((digits_count / 2) as u32);
             let first = elem / split;
             let second = elem % split;
-            result = dp(first, iterations - 1, dp_map) + dp(second, iterations - 1, dp_map);
+            result = find_stone_count(first, iterations - 1, dp_map)
+                + find_stone_count(second, iterations - 1, dp_map);
         } else {
-            result = dp(elem * 2024, iterations - 1, dp_map);
+            result = find_stone_count(elem * 2024, iterations - 1, dp_map);
         }
     }
 
     dp_map.insert((elem, iterations), result);
-
-    // println!("{:?}", dp_map);
-
     result
-}
-
-fn num_digits(num: usize) -> usize {
-    (num as f64).log10().floor() as usize + 1
-}
-
-fn num_digits_u128(num: u128) -> usize {
-    (num as f64).log10().floor() as usize + 1
 }
 
 #[cfg(test)]
@@ -119,40 +91,21 @@ mod tests {
         String::from("125 17")
     }
 
-
     #[test]
-    fn test_exercise_example_part_one() {
+    fn test_exercise_example_input() {
         assert_eq!(run_part_one(&example_input(), 6), 22);
         assert_eq!(run_part_one(&example_input(), 25), 55312);
+        assert_eq!(run_part_two(&example_input(), 6), 22);
+        assert_eq!(run_part_two(&example_input(), 25), 55312);
     }
 
     #[test]
     fn test_input_part_one() {
-        assert_eq!(run_part_one(&utils::read_input(11), 25), 550);
-    }
-
-    #[test]
-    fn test_exercise_example_part_two() {
-        assert_eq!(run_part_two(&example_input(), 6), 22);
+        assert_eq!(run_part_one(&utils::read_input(11), 25), 204022);
     }
 
     #[test]
     fn test_input_part_two() {
-        assert_eq!(run_part_two(&utils::read_input(11), 75), 1255);
+        assert_eq!(run_part_two(&utils::read_input(11), 75), 241651071960597);
     }
 }
-
-/*
-0
-1
-2024
-20 24
-2 0 2 4
-4048, 1, 4048, 8096
-40, 48, 2024, 40, 48, 80, 96
-4, 0, 4, 8, 20, 24, 4, 0, 4, 8, 8, 0, 9, 6
-8096, 1, 8096, 16192, 2, 0, 2, 4, 8096, 1, 8096, 16192, 16192, 1, 18216, 12144
-80, 96, 2024, 80, 96, 32772608, 4048, 1, 4048, 8096, 80, 96, 2024, 80, 96, 32772608, 32772608, 2024, 36869184, 24579456
-8, 0, 9, 6, 20, 24, 8, 0, 9, 6, 3277, 2608, 40, 48, 2024, 40, 48, 80, 96, 8, 0, 9, 6, 20, 24, 8, 0, 9, 6, 3277, 2608, 3277, 2608, 20, 24, 3686, 9184, 2457, 9456
-*/
-
