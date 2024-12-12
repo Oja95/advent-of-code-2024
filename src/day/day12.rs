16f602a31f4de80571f1e4d7dde9afcb8136de1d
Tiit Oja <tiit.oja@xolo.io>
2024-12-12 14:46:10 +0200

    Solution for day 12 part two

--- src/day/day12.rs
+++ src/day/day12.rs
@@ -1,5 +1,7 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::ops::Index;
 use glam::{IVec2, UVec2};
+use itertools::Itertools;
 use crate::day::utils;
 use crate::day::utils::Matrix;
 
@@ -71,7 +73,156 @@ fn run_part_one(input_string: &str) -> usize {
 }
 
 fn run_part_two(input_string: &str) -> usize {
-    1
+    let mut matrix = Matrix::new();
+
+    input_string.lines()
+        .for_each(|line| matrix.push(line.chars().into_iter().collect()));
+
+    let mut visited = vec![vec![false; matrix[0].len()]; matrix.len()];
+
+    let mut regions = Vec::new();
+    // Store regions as pair of area and list of fence positions instead
+    //   -> Vec<(usize, Vec<(IVec2,IVec2)>)>
+
+    for (y, row) in matrix.iter().enumerate() {
+        for (x, elem) in row.iter().enumerate() {
+
+            if !visited[y][x] {
+                let mut section_area = 0;
+                // todo: does it need to be a set instead?
+                let mut section_perimeter:Vec<(IVec2, IVec2)> = Vec::new();
+
+                let mut queue = VecDeque::new();
+                queue.push_back(IVec2 { x: x as i32, y: y as i32 });
+
+                while let Some(position) = queue.pop_front() {
+                    if visited[position.y as usize][position.x as usize] {
+                        continue;
+                    }
+                    visited[position.y as usize][position.x as usize] = true;
+                    section_area += 1;
+
+                    for delta in NEIGHBOUR_DELTAS {
+                        let neighbour = position + delta;
+                        if neighbour.x < 0 || neighbour.y < 0 || neighbour.y >= matrix.len() as i32 || neighbour.x >= matrix[0].len() as i32 {
+                            section_perimeter.push((position, delta));
+                            continue;
+                        }
+
+                        if matrix[neighbour.y as usize][neighbour.x as usize] == matrix[position.y as usize][position.x as usize] {
+                            if !visited[neighbour.y as usize][neighbour.x as usize] {
+                                queue.push_back(neighbour);
+                            }
+                        } else {
+                            section_perimeter.push((position, delta));
+                        }
+                    }
+                }
+                regions.push((section_area, section_perimeter));
+            }
+        }
+    }
+
+    let mut sum = 0;
+
+    for (area, perimeter_sections) in regions {
+        let mut fences_count = 0;
+        let mut visited_sections = vec![false; perimeter_sections.len()];
+
+        // silly way of grouping perimeter sections by adjacency and direction (delta) to get
+        // fence counts, pm me better way to aggregate
+        for i in 0..perimeter_sections.len() {
+            if visited_sections[i] {
+                continue;
+            }
+            let current = perimeter_sections[i];
+            let is_horizontal_fence = current.1.x == 0;
+            fences_count += 1;
+
+            // find all matching positions horizontally with same delta
+            let adjacent_perimeters = perimeter_sections.iter().enumerate()
+                .filter(|(j, elem)| elem.1 == current.1)
+                .filter(|(j, elem)| {
+                    if is_horizontal_fence {
+                        elem.0.y == current.0.y
+                    } else {
+                        elem.0.x == current.0.x
+                    }
+                })
+                .sorted_by(|first, second| {
+                    if is_horizontal_fence {
+                        // if delta x = 0, then horizontal fence and order by X
+                        Ord::cmp(&first.1.0.x, &second.1.0.x)
+                    } else {
+                        // if delta y = 0, then vertical fence and order by Y
+                        Ord::cmp(&first.1.0.y, &second.1.0.y)
+                    }
+                }).collect_vec();
+
+            // if there is a bug up until here then idno wat do
+
+            // find only adjacent in the list
+            let current_index = adjacent_perimeters.iter()
+                .position(|&x| *x.1 == current).unwrap();
+
+            let mut current_x = current.0.x;
+            let mut current_y = current.0.y;
+
+            for j in current_index+1..adjacent_perimeters.len() {
+                if is_horizontal_fence {
+                    // x diff is 1
+                    if adjacent_perimeters[j].1.0.x - current_x == 1 {
+                        visited_sections[adjacent_perimeters[j].0] = true;
+                        current_x += 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    // y diff is 1
+                    if adjacent_perimeters[j].1.0.y - current_y == 1 {
+                        visited_sections[adjacent_perimeters[j].0] = true;
+                        current_y += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            current_x = current.0.x;
+            current_y = current.0.y;
+
+            for j in (0..current_index).rev() {
+                if is_horizontal_fence {
+                    // x diff is 1
+                    if adjacent_perimeters[j].1.0.x - current_x == -1 {
+                        visited_sections[adjacent_perimeters[j].0] = true;
+                        current_x -= 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    // y diff is 1
+                    if adjacent_perimeters[j].1.0.y - current_y == -1 {
+                        visited_sections[adjacent_perimeters[j].0] = true;
+                        current_y -= 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+
+            visited_sections[i] = true;
+        }
+
+        // println!("");
+
+        // sum += area * fences_count;
+        // println!("area: {}, fences: {}", area, fences_count);
+        sum += area * fences_count;
+    }
+
+    sum
 }
 
 #[cfg(test)]
