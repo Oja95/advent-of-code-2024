80dc66d89b754d23257f20a443003b049d0b5a67
Tiit Oja <tiit.oja@xolo.io>
2024-12-16 16:05:21 +0200

    Refactor day 16

--- src/day/day16.rs
+++ src/day/day16.rs
@@ -1,7 +1,7 @@
 use crate::day::utils;
 use glam::IVec2;
 use itertools::Itertools;
-use std::cmp::{min, Ordering};
+use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
 
 pub fn run() {
@@ -18,6 +18,7 @@ struct State {
     path: Vec<IVec2>,
 }
 
+// Make Rust std max-heap implementation into min heap instead
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
         other.cost.cmp(&self.cost)
@@ -31,70 +32,21 @@ impl PartialOrd for State {
 }
 
 fn run_part_one(input_string: &str) -> usize {
-    let mut start_pos = Default::default();
-    let mut end_pos = Default::default();
-
-    let deltas = vec![IVec2::from((0, -1)), IVec2::from((1, 0)), IVec2::from((0, 1)), IVec2::from((-1, 0))];
-
-    let map = input_string.lines().enumerate()
-        .map(|(y, line)| {
-            line.chars().enumerate().map(|(x, c)| {
-                match c {
-                    'S' => { start_pos = IVec2::new(x as i32, y as i32) },
-                    'E' => { end_pos = IVec2::new(x as i32, y as i32) },
-                    _ => {}
-                }
-                c
-            }).collect_vec()
-        }).collect_vec();
-
-    let mut min_heap = BinaryHeap::new();
-    let starting_delta = IVec2::from((1, 0));
-    min_heap.push(State {cost: 0, coords: start_pos, direction: starting_delta, path: vec![]});
-
-    let mut costs: HashMap<(IVec2, IVec2), usize> = HashMap::new();
-    costs.insert((start_pos, starting_delta), 0);
-
-    let mut min_cost = usize::MAX;
-
-    while let Some(state) = min_heap.pop() {
-        if state.coords == end_pos {
-            min_cost = min(state.cost, min_cost);
-        }
-
-        if let Some(&min_cost) = costs.get(&(state.coords, state.direction)) {
-            if state.cost > min_cost {
-                continue;
-            }
-        }
-
-        // directions to process : current + left + right
-        let x1 = deltas.iter().position(|n| *n == state.direction).unwrap() as isize;
-        let new_directions = vec![deltas.get(x1 as usize).unwrap(), deltas.get((4 - (x1 + 1)) as usize).unwrap(), deltas.get(((4 - (x1 - 1)) % 4) as usize).unwrap()];
-
-        for new_direction in new_directions {
-            let new_pos = state.coords + new_direction;
-
-            if map[new_pos.y as usize][new_pos.x as usize] == '#' {
-                continue;
-            }
-
-            let turn_penalty = if state.direction != *new_direction { 1000 } else { 0 };
-            let new_cost = state.cost + 1 + turn_penalty;
-
-            if new_cost < *costs.get(&(new_pos, *new_direction)).unwrap_or(&usize::MAX) {
-                costs.insert((new_pos, *new_direction), new_cost);
-                min_heap.push(State {cost: new_cost, coords: new_pos, direction: *new_direction, path: vec![]});
-            }
-        }
-    }
-
-    min_cost
+    search(input_string).0
 }
 
 fn run_part_two(input_string: &str) -> usize {
+    search(input_string).1.into_iter()
+         .flatten()
+         .unique()
+         .collect_vec()
+         .len()
+}
+
+fn search(input_string: &str) -> (usize, Vec<Vec<IVec2>>) {
     let mut start_pos = Default::default();
     let mut end_pos = Default::default();
+    let starting_delta = IVec2::from((1, 0));
 
     let deltas = vec![IVec2::from((0, -1)), IVec2::from((1, 0)), IVec2::from((0, 1)), IVec2::from((-1, 0))];
 
@@ -111,21 +63,23 @@ fn run_part_two(input_string: &str) -> usize {
         }).collect_vec();
 
     let mut min_heap = BinaryHeap::new();
-    let starting_delta = IVec2::from((1, 0));
     min_heap.push(State {cost: 0, coords: start_pos, direction: starting_delta, path: vec![start_pos]});
 
     let mut costs: HashMap<(IVec2, IVec2), usize> = HashMap::new();
     costs.insert((start_pos, starting_delta), 0);
 
     let mut min_cost = usize::MAX;
-
-    let mut paths_to_position:HashMap<IVec2, HashMap<usize, Vec<Vec<IVec2>>>> = HashMap::new();
-    paths_to_position.insert(start_pos, HashMap::from([(0, vec![vec![start_pos]])]));
+    let mut best_paths = Vec::new();
 
     while let Some(state) = min_heap.pop() {
         if state.coords == end_pos {
             if state.cost < min_cost {
                 min_cost = state.cost;
+                best_paths.clear();
+            }
+
+            if state.cost == min_cost {
+                best_paths.push(state.path.clone());
             }
 
             continue;
@@ -150,26 +104,16 @@ fn run_part_two(input_string: &str) -> usize {
             let turn_penalty = if state.direction != *new_direction { 1000 } else { 0 };
             let new_cost = state.cost + 1 + turn_penalty;
 
-            let mut new_path = state.path.clone();
-            new_path.push(new_pos);
-
-            let cost_paths = paths_to_position.entry(new_pos).or_insert(HashMap::new());
-            let paths = cost_paths.entry(new_cost).or_insert_with(Vec::new);
-            paths.push(new_path.clone());
-
             if new_cost <= *costs.get(&(new_pos, *new_direction)).unwrap_or(&usize::MAX) {
                 costs.insert((new_pos, *new_direction), new_cost);
+                let mut new_path = state.path.clone();
+                new_path.push(new_pos);
                 min_heap.push(State {cost: new_cost, coords: new_pos, direction: *new_direction, path: new_path });
             }
         }
     }
 
-    paths_to_position.get(&end_pos).unwrap()
-        .get(&min_cost).unwrap().into_iter()
-        .flatten()
-        .unique()
-        .collect_vec()
-        .len()
+    (min_cost, best_paths)
 }
 
 #[cfg(test)]
